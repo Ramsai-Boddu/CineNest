1c24fd222d3679afbfe87db45a95acb058a564a1
ganeshoggu31261 <2200031261@kluniversity.in>
2026-04-01 18:22:50 +0530

    my local changes

--- backend/routes/adminRoute.ts
+++ backend/routes/adminRoute.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import { login } from '../controller/adminController';
-import { addMovie } from '../controller/movieController';
+import { addMovie, getMoviesByUser } from '../controller/movieController';
 
 const router = express.Router();
 
 router.put('/login', login);
 router.post("/add-movie",  addMovie);
+router.get("/get-movie/:userId", getMoviesByUser);
 
 export default router;
